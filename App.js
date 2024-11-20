3e643b34a3f7a0c5013268124a5c036a8e026dd2
Lucy Adriana <adrilucy1996@gmail.com>
2024-11-20 17:43:06 -0300

    feat(p2-pdm): adiciona botao que gera imagem

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet } from 'react-native';
+import Button from './components/botao.jsx'
+ 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button></Button>
     </View>
   );
 }
@@ -13,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'end',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
